100630a85c5b98ce553ec4aa89b401e68f7fd649
yudhisteer <yudhisteer.chin@gmail.com>
2025-08-05 23:11:53 -0700

    feat: added script to download data with voxel51

---
+++ dataset/get_data.py
@@ -0,0 +1,31 @@
+import fiftyone as fo
+
+
+def download_dataset(split: str, classes: list[str], max_samples: int) -> fo.Dataset:
+    dataset = fo.zoo.load_zoo_dataset(
+        "open-images-v7",
+        split=split,
+        label_types=["detections"],
+        classes=classes,
+        max_samples=max_samples,
+    )
+    return dataset
+
+
+def main():
+    train_dataset = download_dataset("train", ["Plastic bag"], 500)
+    val_dataset = download_dataset("validation", ["Plastic bag"], 10)
+
+    train_dataset.export(
+        export_dir="dataset/train",
+        dataset_type=fo.types.COCODetectionDataset,
+    )
+
+    val_dataset.export(
+        export_dir="dataset/validation",
+        dataset_type=fo.types.COCODetectionDataset,
+    )   
+
+
+if __name__ == "__main__":
+    main()
