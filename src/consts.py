89bcc47e34d9eb21d57593651f0aac0d0a3b6b3e
yudhisteer <cyudhist@amazon.com>
2025-08-06 11:44:07 -0700

    feat: set constants

---
+++ src/consts.py
@@ -0,0 +1,5 @@
+VALIDATION_LABEL_PATH = "dataset/validation/labels.json"
+TRAIN_LABEL_PATH = "dataset/train/labels.json"
+
+TRAIN_DATA_IMAGES = "dataset/train/data"
+VALIDATION_DATA_IMAGES = "dataset/validation/data"
