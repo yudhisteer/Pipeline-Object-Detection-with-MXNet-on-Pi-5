d424fbf47f7a9008fe6a4aa6ba03dcc6262cc132
yudhisteer <cyudhist@amazon.com>
2025-08-12 16:11:04 -0700

    chore: remove unused files

--- src/consts.py
+++
@@ -1,7 +0,0 @@
-VALIDATION_LABEL_PATH = "dataset/validation/labels.json"
-TRAIN_LABEL_PATH = "dataset/train/labels.json"
-
-TRAIN_DATA_IMAGES = "dataset/train/data"
-VALIDATION_DATA_IMAGES = "dataset/validation/data"
-
-CLASS_NAME = 'plastic_bag'
